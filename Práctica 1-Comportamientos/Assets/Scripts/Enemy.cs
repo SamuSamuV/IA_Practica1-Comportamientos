5eea4e45a4e0f62abefa9c186f8d2f67db41c5e1
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2024-10-16 19:51:42 +0200

    El enemigo mira al jugador cuando le persigue

--- Práctica 1-Comportamientos/Assets/Scripts/Enemy.cs
+++ Práctica 1-Comportamientos/Assets/Scripts/Enemy.cs
@@ -57,60 +57,65 @@ using UnityEngine;
                 else if (hit.collider.CompareTag("ConoVision"))
                 {
                     Debug.DrawRay(player.position, directionToEnemy * raycastDistance, Color.green);
+                    FollowPlayer();
                 }
             }
         }
 
-        //public Transform player;
-        //public Transform ojos;
+    public void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(this.transform.position, player.position, 2 * Time.deltaTime);
 
-        //[Range(0f, 360f)]
-        //public float visionAngle = 30f;
-        //public float visionDistance = 10f;
-
-        //bool detected = false;
-
-        //private void Update()
-        //{
-        //    detected = false;
-        //    Vector2 playerVector = player.position - ojos.position;
-        //    if(Vector3.Angle(playerVector.normalized, ojos.right) < visionAngle * 0.5f)
-        //    {
-        //        detected = true;
-        //    }
-
-        //}
-
-        //private void OnDrawGizmos()
-        //{
-        //    if (visionAngle <= 0f) return;
-
-        //    float halfVisionAngle = visionAngle * 0.5f;
-
-        //    Vector2 p1, p2;
-
-        //    p1 = PointForAngle(halfVisionAngle, visionDistance);
-        //    p2 = PointForAngle(halfVisionAngle, visionDistance);
-
-
-        //    Gizmos.color = detected ? Color.green : Color.red;
-        //    Gizmos.DrawLine(player.position, (Vector2)player.position + p1);
-        //    Gizmos.DrawLine(player.position, (Vector2)player.position + p2);
-
-        //    Gizmos.DrawRay(player.position, ojos.right * 4f);
-        //}
-
-        //Vector2 PointForAngle(float angle, float distance)
-        //{
-        //    return ojos.TransformDirection(
-        //        new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad),
-        //        Mathf.Sin(angle * Mathf.Deg2Rad))
-        //        * distance);
-
-
-        //}
-
+        Vector3 directionToPlayer = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.LookAt(directionToPlayer);
     }
+}
+
+//public Transform player;
+//public Transform ojos;
+
+//[Range(0f, 360f)]
+//public float visionAngle = 30f;
+//public float visionDistance = 10f;
+
+//bool detected = false;
+
+//private void Update()
+//{
+//    detected = false;
+//    Vector2 playerVector = player.position - ojos.position;
+//    if(Vector3.Angle(playerVector.normalized, ojos.right) < visionAngle * 0.5f)
+//    {
+//        detected = true;
+//    }
+
+//}
+
+//private void OnDrawGizmos()
+//{
+//    if (visionAngle <= 0f) return;
+
+//    float halfVisionAngle = visionAngle * 0.5f;
+
+//    Vector2 p1, p2;
+
+//    p1 = PointForAngle(halfVisionAngle, visionDistance);
+//    p2 = PointForAngle(halfVisionAngle, visionDistance);
 
 
+//    Gizmos.color = detected ? Color.green : Color.red;
+//    Gizmos.DrawLine(player.position, (Vector2)player.position + p1);
+//    Gizmos.DrawLine(player.position, (Vector2)player.position + p2);
 
+//    Gizmos.DrawRay(player.position, ojos.right * 4f);
+//}
+
+//Vector2 PointForAngle(float angle, float distance)
+//{
+//    return ojos.TransformDirection(
+//        new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad),
+//        Mathf.Sin(angle * Mathf.Deg2Rad))
+//        * distance);
+
+
+//}
