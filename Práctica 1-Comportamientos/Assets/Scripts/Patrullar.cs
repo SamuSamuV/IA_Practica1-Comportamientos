343643b09302430fecbc5f9440025de2903385f4
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2024-10-17 15:41:59 +0200

    avance de la accion patrullar

--- Práctica 1-Comportamientos/Assets/Scripts/Patrullar.cs
+++ Práctica 1-Comportamientos/Assets/Scripts/Patrullar.cs
@@ -6,17 +6,39 @@ public class Patrullar : MonoBehaviour
 {
     [SerializeField] private float VelocidadMovimiento;
 
-    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private Transform[] puntosMovimiento;//lugares x donde se mueven los enemigos
 
-    [SerializeField] private float[] DistanciaMinima;
+    [SerializeField] private float DistanciaMinima; //q no llegue justo al punto
 
-    int seleccionPunto;
+    int seleccionPunto; // a q punto se va a mover
+    private SpriteRenderer spriteRendenderer;// para controlar como se ve el personaje
+
+
+    public void Start()
+    {
+        seleccionPunto = Random.Range(0,puntosMovimiento.Length);
+        spriteRendenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
+        
         transform.position = Vector3.MoveTowards(transform.position, puntosMovimiento[seleccionPunto].position, VelocidadMovimiento * Time.deltaTime);
 
-        if(Vector3.Distance(transform.position, puntosMovimiento[seleccionPunto].position < DistanciaMinima)
+        if (Vector3.Distance(transform.position, puntosMovimiento[seleccionPunto].position) < DistanciaMinima)
+            
+        {
+
+            seleccionPunto = Random.Range(0, puntosMovimiento.Length);
+        }
+
+        
+    }
+
+
+    private void Girar()
+    {
+        if (transform.position.x < (puntosMovimiento[seleccionPunto].position.x))
         {
 
         }
