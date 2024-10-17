025e7deb575a5ab4096d43008a7c7d005ea657c4
Maryamhidalgo <maryamhidalgo@gmail.com>
2024-10-17 22:16:35 +0200

    wayPoint implementado
    Hay que coordinar patrullar y perseguir porque se pisan. He usado el navMesh

--- Práctica 1-Comportamientos/Assets/Scripts/Patrullar.cs
+++ Práctica 1-Comportamientos/Assets/Scripts/Patrullar.cs
@@ -1,46 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Patrullar : MonoBehaviour
 {
-    [SerializeField] private float VelocidadMovimiento;
 
-    [SerializeField] private Transform[] puntosMovimiento;//lugares x donde se mueven los enemigos
+    private NavMeshAgent enemyAgent; //respete la maya de navegación
+    public Transform[] waypoints;
+    int currentWaypoint = 0;
+    private bool rutaCompleta = false;
 
-    [SerializeField] private float DistanciaMinima; //q no llegue justo al punto
+    //[SerializeField] private float VelocidadMovimiento;
 
-    int seleccionPunto; // a q punto se va a mover
-    private SpriteRenderer spriteRendenderer;// para controlar como se ve el personaje
+    //[SerializeField] private Transform[] puntosMovimiento;//lugares x donde se mueven los enemigos
+
+    //[SerializeField] private float DistanciaMinima; //q no llegue justo al punto
+
+    //int seleccionPunto; // a q punto se va a mover
+    //private SpriteRenderer spriteRendenderer;// para controlar como se ve el personaje
+
+    //private bool hasRotated = false;
+
+
+    //public void Start()
+    //{
+    //    seleccionPunto = Random.Range(0,puntosMovimiento.Length);
+    //    spriteRendenderer = GetComponent<SpriteRenderer>();
+
+
+    //    enemyAgent = GetComponent<NavMeshAgent>();
+    //}
+
+    //private void Update()
+    //{
+    //    enemyAgent.destination = Vector3.zero;
+
+
+    //    transform.position = Vector3.MoveTowards(transform.position, puntosMovimiento[seleccionPunto].position, VelocidadMovimiento * Time.deltaTime);
+
+    //    if (Vector3.Distance(transform.position, puntosMovimiento[seleccionPunto].position) < DistanciaMinima)
+
+    //    {
+
+    //        seleccionPunto = Random.Range(0, puntosMovimiento.Length);
+    //        Girar();
+    //    }
+
+
+    //}
+
+
+    //private void Girar()
+    //{
+    //    if (transform.position.x < (puntosMovimiento[seleccionPunto].position.x))
+    //    {
+    //        transform.Rotate(0, 180, 0);
+    //        hasRotated = true;
+    //    }
+    //}
+
 
 
     public void Start()
     {
-        seleccionPunto = Random.Range(0,puntosMovimiento.Length);
-        spriteRendenderer = GetComponent<SpriteRenderer>();
+        enemyAgent = GetComponent<NavMeshAgent>();//la maya que hemos creado
+
+
+        if (waypoints.Length > 0)
+        {
+            enemyAgent.SetDestination(waypoints[currentWaypoint].position);
+        }
+
     }
+
 
     private void Update()
     {
-        
-        transform.position = Vector3.MoveTowards(transform.position, puntosMovimiento[seleccionPunto].position, VelocidadMovimiento * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, puntosMovimiento[seleccionPunto].position) < DistanciaMinima)
+        if (!enemyAgent.pathPending && enemyAgent.remainingDistance < 0.5f) //si el agente NO está actualmente calculando una nueva ruta o si la distancia de llegada al punto es menor a 0.5 unidades es la distancia mínima considerada como “llegada”.
+        {
             
-        {
+            if (!rutaCompleta)
+            {
+                currentWaypoint++;
+                if (currentWaypoint >= waypoints.Length)
+                {
+                    currentWaypoint = waypoints.Length - 1;
+                    rutaCompleta = true;
+                }
+            }
 
-            seleccionPunto = Random.Range(0, puntosMovimiento.Length);
+            else
+            {
+                currentWaypoint--;
+                if (currentWaypoint < 0)
+                {
+                    currentWaypoint = 0;
+                    rutaCompleta = false;
+                }
+            }
+
+            enemyAgent.SetDestination(waypoints[currentWaypoint].position);
         }
-
-        
     }
 
 
-    private void Girar()
-    {
-        if (transform.position.x < (puntosMovimiento[seleccionPunto].position.x))
-        {
 
-        }
-    }
+
 }
